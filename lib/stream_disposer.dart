ed2402792b88bae7ec79350945dea74344efa689
ssk <developer.kss@gmail.com>
2024-12-31 13:17:59 +0900

    StreamDisposer

--- lib/stream_disposer.dart
+++ lib/stream_disposer.dart
@@ -1,5 +1,22 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'package:meta/meta.dart';
+
+mixin class StreamDisposer {
+  final List<StreamSubscription> _subscriptions = [];
+
+  void addSubscription(StreamSubscription subscription) {
+    _subscriptions.add(subscription);
+  }
+
+  void addSubscriptions(Iterable<StreamSubscription> subscriptions) {
+    _subscriptions.addAll(subscriptions);
+  }
+
+  @mustCallSuper
+  void dispose() {
+    for (var subscription in _subscriptions) {
+      subscription.cancel();
+    }
+    _subscriptions.clear();
+  }
 }
--- test/stream_disposer_test.dart
+++ test/stream_disposer_test.dart
@@ -1,12 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:stream_disposer/stream_disposer.dart';
 
+class MockObject with StreamDisposer {
+  void onStreamListen(void Function(int) onData) {
+    final streamSubscription =
+        Stream<int>.periodic(const Duration(seconds: 1), (int count) => count)
+            .listen(onData);
+    addSubscription(streamSubscription);
+  }
+}
+
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    final mockObject = MockObject();
+
+    mockObject.onStreamListen((int count) {
+      print('count: $count');
+    });
+
+    mockObject.dispose();
+
   });
 }
